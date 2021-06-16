56302cac1b9f9ff5402563160370865b0d3a75c0
Jeremy Zongker <jeremy@trilitech.com>
2021-06-16 00:15:13 -0500

    Api base cleanup (#1)
    * Removed apibase
    
    * Added apibase as submodule

--- src/apiBase/models/Page.ts
+++
@@ -1,8 +0,0 @@
-export class Page {
-    id?: string;
-    churchId: string;
-    name: string;
-    content: string;
-    path: string;
-    lastModified?: Date;
-}
--- src/apiBase/models/Setting.ts
+++
@@ -1,7 +0,0 @@
-export class Setting {
-    public id?: string;
-    public churchId?: string;
-    public keyName?: string;
-    public value?: string;
-    public public?: string
-}

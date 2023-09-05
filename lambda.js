74ef3b4149d15c70511310897def09b787b9dce0
Jeremy Zongker <jeremy@zongker.net>
2023-09-04 20:20:33 -0500

    Fixed lambda entry

--- lambda.js
+++ lambda.js
@@ -2,13 +2,18 @@ const { createServer, proxy } = require('aws-serverless-express');
 const { init } = require('./dist/App');
 const { Environment } = require('./dist/helpers');
 
-Environment.init(process.env.APP_ENV);
-//Pool.initPool();
+
+const checkPool = async () => {
+  if (!Environment.connectionString) {
+    await Environment.init(process.env.APP_ENV)
+  }
+}
 
 module.exports.universal = function universal(event, context) {
-  init().then(app => {
-    const server = createServer(app);
-    return proxy(server, event, context);
+  checkPool().then(() => {
+    init().then(app => {
+      const server = createServer(app);
+      return proxy(server, event, context);
+    });
   });
-
 }
